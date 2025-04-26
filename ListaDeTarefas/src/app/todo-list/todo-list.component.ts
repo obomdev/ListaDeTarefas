fadedf5456dba4efb273a7eec570628d1d4fa80a
obomdev <vidaldevpro@gmail.com>
2025-04-25 21:07:11 -0300

    Inicio do Projeto Angular e criado interface inicial do gerenciamento de tarefas

---
+++ ListaDeTarefas/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent {
+
+  onSubmit(form: NgForm){
+    
+  }
+
+}
